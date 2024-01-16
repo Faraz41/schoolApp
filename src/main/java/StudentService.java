057b74ae9f49c4858a2b27e3fa4ffac6342b7e5d
Faraz Masood <m.faraz118@live.com>
2024-01-15 19:34:27 -0500

    add an interface for CRUD services

---
+++ src/main/java/StudentService.java
@@ -0,0 +1,12 @@
+public interface StudentService {
+    // CRUD
+    // Create
+    // Read
+    // Update
+    // Delete
+    void addStudent (Student student);
+    void readStudents ();
+    void updateStudent();
+    void deleteStudent();
+    // above is interface after this we provide implementation.
+}
